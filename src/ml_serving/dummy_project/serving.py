65675eabc90a5bae01d01230bacd31195a20c156
Ben0112358 <111197969+Ben0112358@users.noreply.github.com>
2025-07-14 19:47:33 +0200

    initial structure + dummy project (#1)
    * initial poetry project stuff
    
    * github workflow
    
    * minimal working dummy project; will be improved
    
    * linting

---
+++ src/ml_serving/dummy_project/serving.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+import joblib
+from ml_serving.config import MODEL_DIR
+import numpy as np
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.model = joblib.load(MODEL_DIR / "model.pkl")
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+class ModelPredictRequest(BaseModel):
+    data: list[float] | list[int]
+
+
+@app.post("/predict")
+async def predict(model_input: ModelPredictRequest, request: Request):
+    try:
+        model = request.app.state.model
+        predictions = model.predict(np.array(model_input.data).reshape(-1, 1))
+        return {"predictions": predictions.ravel().tolist()}
+    except Exception as e:
+        return {"error": str(e)}
