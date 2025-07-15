8c8891918161b97a087f784aa99fd7e4d3ec9daa
Ben0112358 <111197969+Ben0112358@users.noreply.github.com>
2025-07-15 13:50:40 +0200

    Add very basic live logging support (#2)
    * basic logging working for the serving, with some error handling
    
    * linting + fix line length for linting

--- src/ml_serving/dummy_project/serving.py
+++ src/ml_serving/dummy_project/serving.py
@@ -1,22 +1,48 @@
 from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from fastapi.exceptions import RequestValidationError
+from fastapi.exception_handlers import request_validation_exception_handler
+from pydantic import BaseModel, field_validator
 from contextlib import asynccontextmanager
 import joblib
 from ml_serving.config import MODEL_DIR
 import numpy as np
+from .utils import setup_logging
+import logging
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    setup_logging()
     app.state.model = joblib.load(MODEL_DIR / "model.pkl")
+    app.state.logger = logging.getLogger(__name__)
     yield
 
 
 app = FastAPI(lifespan=lifespan)
 
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request, exc: RequestValidationError
+):
+
+    app.state.logger.error(
+        f"Validation failed for request: " f"{await request.body()}"
+    )
+
+    app.state.logger.error(f"Validation errors: {exc.errors()}")
+    return await request_validation_exception_handler(request, exc)
+
+
 class ModelPredictRequest(BaseModel):
     data: list[float] | list[int]
+
+    @field_validator("data")
+    @classmethod
+    def not_empty(cls, v):
+        if not v:
+            raise ValueError("Input data must not be empty.")
+        return v
 
 
 @app.post("/predict")
@@ -24,6 +50,10 @@ async def predict(model_input: ModelPredictRequest, request: Request):
     try:
         model = request.app.state.model
         predictions = model.predict(np.array(model_input.data).reshape(-1, 1))
+        app.state.logger.info(f"Input: {model_input}, Output: {predictions}")
         return {"predictions": predictions.ravel().tolist()}
     except Exception as e:
+        app.state.logger.info(
+            f"Predict failed for input: {model_input}. " f"Error: {str(e)}"
+        )
         return {"error": str(e)}
---
+++ src/ml_serving/dummy_project/utils/__init__.py
@@ -0,0 +1,4 @@
+from .fake_raw_data import generate_fake_raw_dummy_data
+from .logging import setup_logging
+
+__all__ = ["generate_fake_raw_dummy_data", "setup_logging"]
