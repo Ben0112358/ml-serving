54929aff38a2f15aecf1a70cd0e488f8ad9ef725
Ben0112358 <benjaminringdahl@gmail.com>
2025-07-21 12:28:32 +0200

    refactor logging function

--- src/ml_serving/dummy_project/utils/__init__.py
+++ src/ml_serving/dummy_project/utils/__init__.py
@@ -1,4 +1,3 @@
 from .fake_raw_data import generate_fake_raw_dummy_data
-from .logging import setup_logging
 
-__all__ = ["generate_fake_raw_dummy_data", "setup_logging"]
+__all__ = ["generate_fake_raw_dummy_data"]
